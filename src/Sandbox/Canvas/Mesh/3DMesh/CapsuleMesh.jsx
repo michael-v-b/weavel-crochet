596356af17b0acd8cdfa947e1c73078b1ba83c26
mikkel <michael.v.burton@gmail.com>
2025-05-12 12:23:04 -0400

    update capsule to be more accurate

--- src/Sandbox/Canvas/Mesh/3DMesh/CapsuleMesh.jsx
+++ src/Sandbox/Canvas/Mesh/3DMesh/CapsuleMesh.jsx
@@ -1,4 +1,5 @@
-import  { forwardRef, useState, useEffect } from "react";
+import  { forwardRef, useState, useEffect,useMemo } from "react";
+import {BufferAttribute,BufferGeometry} from 'three';
 import SelectableMesh from "../SelectableMesh";
 import useStore from "../../../DevTools/store";
 
@@ -34,6 +35,72 @@ const CapsuleMesh = forwardRef(({ id, ...props }, ref) => {
     
   }, []);
 
+  const inRange = (a, length, bottom) => {
+    return (a % length) + bottom;
+  };
+
+  const segments = 10;
+
+  const geometry = useMemo(() => {
+    const geo = new BufferGeometry();
+    const vertices = [];
+    const indices = [];
+
+    //horizontal circle radius/height
+    for (let i = 0; i < segments+1/*segments + 1*/; i++) {
+      //keeps phi the same on final entry
+      const phi =  (i/segments) * Math.PI;
+      console.log("phi: " + phi);
+      const RING_HEIGHT =
+        i < (segments/2) ? height_convert((height/2))-radius : -((height_convert(height/2)-radius));
+
+      //creates full horizontal circle
+      for (let j = 0; j < segments + 1; j++) {
+        if (j < segments) {
+          const tempRadius = Math.sin(phi) * radius;
+          const theta = (j / segments) * 2 * Math.PI;
+          const x = Math.cos(theta) * tempRadius;
+          const z = Math.sin(theta) * tempRadius;
+          const y = Math.cos(phi) * radius + RING_HEIGHT;
+          vertices.push(x, y, z);
+        }
+
+        if (j > 0 && i > 0) {
+          //a-b
+          //c-d
+
+          //top segments
+          const a = inRange(j - 1, segments, (i - 1) * segments);
+          const b = inRange(j, segments, (i - 1) * segments);
+          const c = inRange(j - 1, segments, i * segments);
+          const d = inRange(j, segments, i * segments);
+          indices.push(a, b, c);
+          indices.push(c, b, d);
+        }
+      }
+    }
+
+    geo.setAttribute(
+      "position",
+      new BufferAttribute(new Float32Array(vertices), 3)
+    );
+
+    geo.setIndex(indices);
+    geo.computeVertexNormals();
+    return geo;
+  }, [height, circum]);
+
+  useEffect(() => {
+    if(!meshLoading) {
+      const newMesh = projectFile.meshes[id];
+      newMesh.attributeList = attributeList;
+      newMesh.height = height;
+      newMesh.circum = circum;
+      newMesh.radius = radius;
+      setProjectFile({ ...projectFile });
+    }
+  }, []);
+
   return (
     <SelectableMesh
       meshType="capsule"
@@ -51,7 +118,8 @@ const CapsuleMesh = forwardRef(({ id, ...props }, ref) => {
       ref={ref}
       {...props}
     >
-      <capsuleGeometry args={[radius,height_convert(height)/2, 10, 10]} />
+      {/*<capsuleGeometry args={[radius,height_convert(height)/2, 10, 10]} />*/}
+      <primitive object = {geometry}/>
     </SelectableMesh>
   );
 });
--- src/Sandbox/UI/InfoWindow/InfoPages/ShapePages/CapsulePage.jsx
+++ src/Sandbox/UI/InfoWindow/InfoPages/ShapePages/CapsulePage.jsx
@@ -14,21 +14,22 @@ const CapsulePage = ({ object}) => {
     const circumRef = useRef(null);
   
     /**
-     * updates the circumference state in order to change the max height.
+     * updates the height state when the circumference changes
      */
     const handleCircum = (newCircum) => {
       const height = heightRef.current.height;
-      const temp = Math.max(height,Math.ceil(newCircum/MAX_RATE));
+      const temp = Math.max(height,Math.ceil(2*newCircum/MAX_RATE));
       heightRef.current.setHeight(temp);
       objectData.setHeight(temp);
     };
   
     /**
-     * Updates the height when the height changes
+     * Updates the circumference when the height changes
      */
     const handleHeight = (newHeight) => {
+      const roundedHeight = Math.floor(newHeight/2);
       const circum = circumRef.current.circum;
-      const temp = Math.min(circum,Math.ceil(newHeight*MAX_RATE));
+      const temp = Math.min(circum,Math.ceil(roundedHeight*MAX_RATE));
       circumRef.current.setCircum(temp);
       objectData.setRadius(circumRef.current.findRadius(temp));
     } 
