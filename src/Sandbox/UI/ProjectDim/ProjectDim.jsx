6e09175bc612a568631a08fee66f2ff223fc5c12
michael-v-b <michael.v.burton@gmail.com>
2025-05-19 16:49:00 -0400

    update dimensions for projectDim

--- src/Sandbox/UI/ProjectDim/ProjectDim.jsx
+++ src/Sandbox/UI/ProjectDim/ProjectDim.jsx
@@ -1,11 +1,63 @@
 import "./ProjectDim.css";
 import {useEffect,useState} from 'react';
+import {motion} from 'framer-motion';
 import useStore from "../../DevTools/store";
 
 const ProjectDim = () => {
   const projectDims = useStore((state) => state.projectDims);
   const [projectText,setProjectText] = useState([0,0,0]);
+  const [metric, setMetric] = useState(false);
 
+
+  const simplify = (num) => {
+    let output = "";
+    if(metric) {
+      output = simplifyMeter(num);
+    } else {
+      output = simplifyFeet(num);
+    }
+    return output;
+  }
+
+  const simplifyFeet = (num)=>{
+        const FOOT = 12;
+    const feet = Math.floor(num/FOOT);
+
+    const inches = parseFloat((num%FOOT).toFixed(2));
+
+    let output = "";
+    if(feet > 0) {
+      output = feet + "' " + inches +"''";
+    } else {
+      output = inches + "''";
+    }
+    return output;
+  }
+
+  const simplifyMeter = (num)=>{
+    let mm = num*25.4;
+    let cm = 0;
+    let m = 0;
+
+    if(mm > 10) {
+      cm = mm/10;
+    }
+
+    if(cm > 100) {
+      m = cm/100;
+    }
+
+    let output = ""
+    if(m != 0) {
+      output = m.toFixed(2) + " m.";
+    } else if (cm != 0) {
+      output = cm.toFixed(2) + "cm.";
+    } else {
+      output = mm.toFixed(2) + "mm.";
+    }
+    return output;
+
+  }
 
 
   useEffect(()=>{
@@ -18,19 +70,18 @@ const ProjectDim = () => {
     
   },[projectDims]);
 
-  useEffect(()=>{
-    console.log(projectText);
-  },[projectText]);
+
   
   return (
-    <div className="project-dim">
+    <motion.div className="project-dim">
       Dimensions:
-      <div className = "project-dim-text">{projectText[0]}</div>
+      <div className = "project-dim-text">{simplify(projectText[0])}</div>
       x
-      <div className = "project-dim-text">{projectText[1]}</div>
+      <div className = "project-dim-text">{simplify(projectText[1])}</div>
       x
-      <div className = "project-dim-text">{projectText[2]}</div>
-    </div>
+      <div className = "project-dim-text">{simplify(projectText[2])}</div>
+      (in.)
+    </motion.div>
   );
 };
 
