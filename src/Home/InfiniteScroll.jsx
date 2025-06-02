b6e0134f39c1de23c8589cd279b4a07dc0de0ffd
michael-v-b <michael.v.burton@gmail.com>
2025-06-02 14:18:09 -0400

    add slider for infinite scroll

--- src/Home/InfiniteScroll.jsx
+++ src/Home/InfiniteScroll.jsx
@@ -3,7 +3,7 @@ import "./InfiniteScroll.css";
 
 const InfiniteScroll = () => {
     const scrollList = [
-        1,2,3,4,5
+        1,2,3,4,5,6
     ]
     const animationTime = 12;
 
@@ -11,13 +11,13 @@ const InfiniteScroll = () => {
         {scrollList.map((value,key) => {
             console.log("key: " + key);
             return <motion.div key = {key} 
-            initial = {{opacity:1}}
-            animate = {{x:'-85vw',opacity:[0,1,1,0]}}
+            initial = {{x:'20vw',opacity:1}}
+            animate = {{x:'-90vw',opacity:[0,1,1,0]}}
             transition = {
                 
                 {
-                x:{ease:"linear",delay: key*(animationTime/5), duration:animationTime,repeat:Infinity},
-                opacity:{ease:"linear",delay:key*(animationTime/5),times:[0,0.2,0.9,1],duration:animationTime,repeat:Infinity}}
+                x:{ease:"linear",delay: key*(animationTime/6), duration:animationTime,repeat:Infinity},
+                opacity:{ease:"linear",delay:key*(animationTime/6),times:[0,0.2,0.9,1],duration:animationTime,repeat:Infinity}}
             }
             className = "scroll-image"> 
                 {value}
