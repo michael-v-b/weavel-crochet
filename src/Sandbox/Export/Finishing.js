31010cac30f014a904440ea5b3359b8ed290d144
mikkel <michael.v.burton@gmail.com>
2025-06-23 16:56:13 -0400

    implement dfs for sewing

--- src/Sandbox/Export/Finishing.js
+++ src/Sandbox/Export/Finishing.js
@@ -3,8 +3,26 @@ import IntersectionMap from "./IntersectionMap";
 const Finishing = (meshList) => {
     const intersectionMap = IntersectionMap(meshList);
 
-    console.log("intersecitonMap");
-    console.dir(intersectionMap);
+    const visited = [];
+
+    const dfs = (name) => {
+        if(visited.includes(name) || !intersectionMap[name]) {
+           return; 
+        } else {
+            visited.push(name);
+        }
+        
+        const intersections = intersectionMap[name];
+        for(let i = 0; i < intersections.length;i++) {
+            dfs(intersections[i]);
+        }
+    }
+    
+    for(let i =0; i <meshList.length;i++ ) {
+        dfs(meshList[i].name);
+    }
+
+    console.log("visited: " + visited);
 }
 
 export default Finishing;
