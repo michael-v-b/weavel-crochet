da8ba68b172cf935baed71293b36348a0a47163d
mikkel <michael.v.burton@gmail.com>
2025-06-30 15:35:55 -0400

    small edit to profile page layout

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -30,46 +30,50 @@ const Profile = () => {
           </div>
 
           <div className="profile-option-container">
-
+  
             <hr className=  "profile-line"/>
 
             {/*RESET PASSWORD ///////////////////////*/}
-            <div className=  "profile-option-title">
-              Password Reset:
+            <div className=  "profile-option-subcontainer">
+              <div className=  "profile-option-title">
+                Password Reset:
+              </div>
+              <div className = "profile-description">
+                Click the button below to reset the password to your account.
+              </div>
+              <motion.div whileHover = {{scale:1.1}} 
+              whileTap = {{scale:0.9}}
+              onClick = {()=>{
+                navigate("/forgot_pass")
+              }}
+              className=  "profile-reset-pass clickable">
+        
+                Reset Password
+              </motion.div>
             </div>
-            <div className = "profile-description">
-              Click the button below to reset the password to your account.
-            </div>
-            <motion.div whileHover = {{scale:1.1}} 
-            whileTap = {{scale:0.9}}
-            onClick = {()=>{
-              navigate("/forgot_pass")
-            }}
-            className=  "profile-reset-pass clickable">
-       
-              Reset Password
-            </motion.div>
 
             <hr className = "profile-line"/>
-            
+
             {/*DELETE ACCOUNT /////////////////////////*/}
-            <div className = "profile-option-title">
-              Delete Account:
+            <div className=  "profile-option-subcontainer">
+              <div className = "profile-option-title">
+                Delete Account:
+              </div>
+              <div className="profile-description">
+                Clicking this will delete your account. It will delete all of your
+                projects, and remove your email from our database.
+              </div>
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => {
+                  setDeleteAccount(true);
+                }}
+                className="profile-option delete-button profile-delete"
+              >
+                Delete Account
+              </motion.div>
             </div>
-            <div className="profile-description">
-              Clicking this will delete your account. It will delete all of your
-              projects, and remove your email from our database.
-            </div>
-            <motion.div
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              onClick={() => {
-                setDeleteAccount(true);
-              }}
-              className="profile-option delete-button profile-delete"
-            >
-              Delete Account
-            </motion.div>
           </div>
         </div>
       </div>
