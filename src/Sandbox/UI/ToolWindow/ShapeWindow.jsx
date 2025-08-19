d3a58c2dd464b6906ea37509e43c62063d6514a3
Michael Burton <michael.v.burton@gmail.com>
2025-08-19 14:40:23 -0400

    add capsule icon

--- src/Sandbox/UI/ToolWindow/ShapeWindow.jsx
+++ src/Sandbox/UI/ToolWindow/ShapeWindow.jsx
@@ -5,6 +5,7 @@ import CircleIcon from "../../../assets/Icons/Shapes/circle.svg?react";
 import ConeIcon from "../../../assets/Icons/Shapes/cone.svg?react";
 import BoxIcon from "../../../assets/Icons/Shapes/cube.svg?react";
 import CylinderIcon from "../../../assets/Icons/Shapes/cylinder.svg?react";
+import CapsuleIcon from "../../../assets/Icons/Shapes/capsule.svg?react";
 import EyeIcon from "../../../assets/Icons/Shapes/eye.svg?react";
 import SiloIcon from "../../../assets/Icons/Shapes/silo.svg?react";
 import SphereIcon from "../../../assets/Icons/Shapes/sphere.svg?react";
@@ -12,32 +13,33 @@ import SquareIcon from "../../../assets/Icons/Shapes/square.svg?react";
 import StadiumIcon from "../../../assets/Icons/Shapes/stadium.svg?react";
 import TriangleIcon from "../../../assets/Icons/Shapes/triangle.svg?react";
 
-
 /**
  *@typedef {ShapeWindow} - Window that showcases all the different shapes that can be added to the scene.
  *@property {GetShapeCallback} - Allows shape that was just spawned to be accessible to parents.
  *@property {Reference} mouseHoverRef - a reference to the mouseHover object
  *@returns {Component} - Div full of buttons representing every shape that can be added, organized by dimension.
  */
-const ShapeWindow = ({ getShape,mouseHoverRef}) => {
+const ShapeWindow = ({ getShape, mouseHoverRef }) => {
   const handleClick = (shape) => {
     getShape(shape);
   };
   const thirdShapes = [
-    ["ball", <SphereIcon className = 'tool-icon'/>],
-    ["silo", <SiloIcon className = 'tool-icon'/>],
-    ["capsule", "Capsule"],
-    ["cone", <ConeIcon className = 'tool-icon'/>],
-    ["cylinder", <CylinderIcon className = 'tool-icon'/>],
-    ["box",<BoxIcon className = 'tool-icon'/>]];
+    ["ball", <SphereIcon className="tool-icon" />],
+    ["silo", <SiloIcon className="tool-icon" />],
+    ["capsule", <CapsuleIcon className="tool-icon" />],
+    ["cone", <ConeIcon className="tool-icon" />],
+    ["cylinder", <CylinderIcon className="tool-icon" />],
+    ["box", <BoxIcon className="tool-icon" />],
+  ];
 
   const secondShapes = [
-    ["chain",  <ChainIcon className = 'tool-icon'/>],
-    ["circle",  <CircleIcon className = 'tool-icon'/>],
-    ["stadium",  <StadiumIcon className = 'tool-icon'/>],
-    ["square",  <SquareIcon className = 'tool-icon'/>],
-    ["triangle", <TriangleIcon className = 'tool-icon'/>],
-    ["eye", <EyeIcon className = 'tool-icon'/>]];
+    ["chain", <ChainIcon className="tool-icon" />],
+    ["circle", <CircleIcon className="tool-icon" />],
+    ["stadium", <StadiumIcon className="tool-icon" />],
+    ["square", <SquareIcon className="tool-icon" />],
+    ["triangle", <TriangleIcon className="tool-icon" />],
+    ["eye", <EyeIcon className="tool-icon" />],
+  ];
   return (
     <>
       <div className="tool-window">
@@ -54,10 +56,10 @@ const ShapeWindow = ({ getShape,mouseHoverRef}) => {
                 onClick={() => {
                   handleClick(name);
                 }}
-                onMouseEnter = {()=>{
+                onMouseEnter={() => {
                   mouseHoverRef.current.startTimer(capitalized);
                 }}
-                onMouseLeave = {()=>{
+                onMouseLeave={() => {
                   mouseHoverRef.current.cancelTimer();
                 }}
               >
@@ -80,10 +82,10 @@ const ShapeWindow = ({ getShape,mouseHoverRef}) => {
                 onClick={() => {
                   handleClick(name);
                 }}
-                onMouseEnter = {()=>{
+                onMouseEnter={() => {
                   mouseHoverRef.current.startTimer(capitalized);
                 }}
-                onMouseLeave = {()=>{
+                onMouseLeave={() => {
                   mouseHoverRef.current.cancelTimer();
                 }}
               >
