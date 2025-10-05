1621c14b4970a79cae097b5637042ea5a951dd72
Michael Burton <michael.v.burton@gmail.com>
2025-10-05 16:52:08 -0400

    update supabase after tutorial

--- src/Sandbox/UI/NameArea/Tutorial/TutorialManager.jsx
+++ src/Sandbox/UI/NameArea/Tutorial/TutorialManager.jsx
@@ -3,10 +3,13 @@ import useStore from "../../../DevTools/store";
 import useRefStore from "../../../DevTools/refStore";
 import TutorialWindow from "./TutorialWindow";
 import TutorialValues from "./TutorialValues";
+import supabase from "../../../../supabase";
+import useGlobalStore from "../../../../globalStore";
 
 const TutorialManager = ({}) => {
   const tutorialActive = useStore((state) => state.tutorialActive);
   const setTutorial = useStore((state) => state.setTutorial);
+  const authData = useGlobalStore((state) => state.authData);
   const refs = useRefStore((state) => state.refs);
   const [currentStep, setStep] = useState(0);
   const tutorialValues = TutorialValues();
@@ -17,7 +20,7 @@ const TutorialManager = ({}) => {
   useEffect(() => {
     if (currentStep >= tutorialValues.length - 1) {
       setTutorial(false);
-    } else {
+      finishTutorial();
     }
   }, [currentStep]);
 
@@ -42,6 +45,22 @@ const TutorialManager = ({}) => {
    */
   const decrementStep = () => {
     setStep(Math.max(currentStep - 1, 0));
+  };
+
+  const finishTutorial = async () => {
+    if (!authData) {
+      return;
+    }
+    const user_id = authData.user.id;
+    const { error } = await supabase
+      .from("Profiles")
+      .update({ finished_tutorial: true })
+      .eq("user_id", user_id);
+
+    if (error) {
+      console.log("error");
+      console.dir(error);
+    }
   };
 
   return (
