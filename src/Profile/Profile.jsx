cdade89f823f27f45fd7a3a9ad23d1e5fed4d575
Michael Burton <michael.v.burton@gmail.com>
2025-11-23 11:36:23 -0500

    test branch update

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import Footer from "../UI/Footer/Footer";
 
 import AuthTester from "../AuthTester";
 import ProfileDelete from "./ProfileDelete/ProfileDelete";
-import {useNavigate} from 'react-router';
+import { useNavigate } from "react-router";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import "./Profile.css";
@@ -21,7 +21,7 @@ const Profile = () => {
       <div className="projects-web-container">
         <Banner />
         <AuthTester reroute="" />
-
+        <div> This is added to test if it's a different branch</div>
         {deleteAccount && <ProfileDelete setDeleteAccount={setDeleteAccount} />}
 
         <div className="projects-container">
@@ -30,38 +30,34 @@ const Profile = () => {
           </div>
 
           <div className="profile-option-container">
-  
             {/*<hr className=  "profile-line"/>*/}
 
             {/*RESET PASSWORD ///////////////////////*/}
-            <div className=  "profile-option-subcontainer">
-              <div className=  "profile-option-title">
-                Password Reset:
-              </div>
-              <div className = "profile-description">
+            <div className="profile-option-subcontainer">
+              <div className="profile-option-title">Password Reset:</div>
+              <div className="profile-description">
                 Click the button below to reset the password to your account.
               </div>
-              <motion.div whileHover = {{scale:1.1}} 
-              whileTap = {{scale:0.9}}
-              onClick = {()=>{
-                navigate("/forgot_pass")
-              }}
-              className=  "profile-reset-pass clickable">
-        
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => {
+                  navigate("/forgot_pass");
+                }}
+                className="profile-reset-pass clickable"
+              >
                 Reset Password
               </motion.div>
             </div>
 
-            <hr className = "profile-line"/>
+            <hr className="profile-line" />
 
             {/*DELETE ACCOUNT /////////////////////////*/}
-            <div className=  "profile-option-subcontainer">
-              <div className = "profile-option-title">
-                Delete Account:
-              </div>
+            <div className="profile-option-subcontainer">
+              <div className="profile-option-title">Delete Account:</div>
               <div className="profile-description">
-                Clicking this will delete your account. It will delete all of your
-                projects, and remove your email from our database.
+                Clicking this will delete your account. It will delete all of
+                your projects, and remove your email from our database.
               </div>
               <motion.div
                 whileHover={{ scale: 1.1 }}
@@ -77,7 +73,7 @@ const Profile = () => {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
